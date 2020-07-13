4f19720a904e215ac116c7f90952ac5446bf703a
Benjamin Roisen <broisen@ucsc.edu>
2020-07-13 16:49:38 -0700

    Added THIRD_HYPOTHESIS state & changing text based on state
    Added an additional scene after realizing I forgot the student sharing portion of the lab. Also made the text for the hypothesis/reasoning text boxes change to show the directions for the lab.

--- scripts/lab.js
+++ scripts/lab.js
@@ -2,15 +2,17 @@
 let username; //name of the user
 let initialHypothesis, initialReasoning; //hypothesis/reasoning from round 1
 let updatedHypothesis, updatedReasoning; //hypothesis/reasoning from round 2
-let finalHypothesis, finalReasoning; //hypothesis/reasoning from round 3
+let thirdHypothesis, thirdReasoning; //hypothesis/reasoning from round 3
+let finalHypothesis, finalReasoning; //hypothesis/reasoning from round 4
 
 //global variables for state tracking
 const states = {
-    SETUP: 0,
-    INITIAL_HYPOTHESIS: 1,
-    UPDATED_HYPOTHESIS: 2,
-    FINAL_HYPOTHESIS: 3,
-    END: 4
+    SETUP: 0,               //initial setup; ask for student's name
+    INITIAL_HYPOTHESIS: 1,  //unlock 4 emails; form 1st tentative hypothesis
+    UPDATED_HYPOTHESIS: 2,  //unlock 4 more emails; form 2nd tentative hypothesis
+    THIRD_HYPOTHESIS: 3,    //unlock 2 final emails; form 3rd tentative hypothesis
+    FINAL_HYPOTHESIS: 4,    //meet w/ other groups to compare data; form a final hypothesis
+    END: 5                  //save user input & unlocked emails to PDF for assignment submission
 };
 let state = states.SETUP; //tracks where we are in the lab
 
@@ -140,9 +142,10 @@ function submitHypothesis()
 {
     switch(state)
     {
-        case states.INITIAL_HYPOTHESIS: //entered 1st hypothesis
+        case states.INITIAL_HYPOTHESIS: //entered 1st hypothesis after seeing 1st 4 emails
             if(checkHypothesis()) //confirm hypothesis/reasoning was entered
             {
+                console.log("Saved 1st hypothesis; unlocking 4 more emails");
                 //save the hypothesis/reasoning
                 initialHypothesis = hypoInputBox.value();
                 initialReasoning = reasonInputBox.value();
@@ -151,34 +154,69 @@ function submitHypothesis()
                 //unlock 4 more emails
                 /*TODO*/
 
+                //update the hypothesis/reasoning labels w/ the prompt for the 2nd hypothesis
+                hypoLabel.html("Using the new data from the 4 new emails, formulate a second tentative hypothesis that explains the storyline.");
+                reasonLabel.html("Explain your reasoning behind the storyline you created, including how you integrated new data into your hypothesis.")
+
                 //move to next state
                 state = states.UPDATED_HYPOTHESIS;
             }
             break;
-        case states.UPDATED_HYPOTHESIS: //entered 2nd hypothesis
+        case states.UPDATED_HYPOTHESIS: //entered 2nd hypothesis after seeing 4 new emails
             if(checkHypothesis(initialHypothesis, initialReasoning)) //confirm hypothesis/reasoning was entered & updated
             {
+                console.log("Saved 2nd hypothesis; unlocking 2 more emails");
                 //save the new hypothesis/reasoning
                 updatedHypothesis = hypoInputBox.value();
                 updatedReasoning = reasonInputBox.value();
                 console.log(updatedHypothesis);
                 console.log(updatedReasoning);
-                //unlock 4 more emails
+                //unlock 2 more emails
                 /*TODO*/
+
+                //update the hypothesis/reasoning labels w/ the prompt for the 3rd hypothesis
+                hypoLabel.html("Using the new data from the 2 new emails, formulate a third tentative hypothesis that explains the storyline.");
+                //reasonLabel.html("Explain your reasoning behind the storyline you created, including how you integrated new data into your hypothesis.")
+
+                //move to next state
+                state = states.THIRD_HYPOTHESIS;
+            }
+            break;
+        case states.THIRD_HYPOTHESIS: //entered 3rd hypothesis after seeing final 2 emails
+            if(checkHypothesis(updatedHypothesis, updatedReasoning)) //confirm hypothesis/reasoning was entered & updated
+            {
+                console.log("Saved 3rd hypothesis; it's time to share!");
+                //save the new hypothesis/reasoning
+                thirdHypothesis = hypoInputBox.value();
+                thirdReasoning = reasonInputBox.value();
+                console.log(thirdHypothesis);
+                console.log(thirdReasoning);
+
+                /* Maybe save to PDF here to facilitate sharing without
+                   relying on the student using screensharing or whatever?*/
+                
+                //update the hypothesis/reasoning labels w/ the prompt for the 4th hypothesis
+                hypoLabel.html("To simulate the collaborative nature of science, take a few minutes to meet with other groups to compare data."
+                     + " Remember, since each group received emails at random, groups may have different data."
+                     + "<br>When you've finished sharing, formulate a final hypothesis based upon all the available data."
+                     + " This hypothesis should attempt to explain the events in the life of the character(s) who wrote the emails.");
+                //reasonLabel.html("Explain your reasoning behind the storyline you created, including how you integrated new data into your hypothesis.")
 
                 //move to next state
                 state = states.FINAL_HYPOTHESIS;
             }
             break;
-        case states.FINAL_HYPOTHESIS: //entered 3rd hypothesis
-            if(checkHypothesis(updatedHypothesis, updatedReasoning)) //confirm hypothesis/reasoning was entered & updated
+        case states.FINAL_HYPOTHESIS: //entered 4th & final hypothesis after sharing w/ another group
+            if(checkHypothesis(thirdHypothesis, thirdReasoning)) //confirm hypothesis/reasoning was entered & updated
             {
+                console.log("Saved final hypothesis; time to print to PDF");
                 //save the new hypothesis/reasoning
                 finalHypothesis = hypoInputBox.value();
                 finalReasoning = reasonInputBox.value();
                 console.log(finalHypothesis);
                 console.log(finalReasoning);
-                //unlock 4 more emails
+                
+                //save to PDF
                 /*TODO*/
 
                 //move to next state
